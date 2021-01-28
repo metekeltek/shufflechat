3b54deea5833b49693e30182bc1bd30b1467d13a
Sempribo <keltekm@googlemail.com>
2021-01-28 01:38:44 +0100

    stuff

---
+++ lib/ui/filterIntrests.dart
@@ -0,0 +1,278 @@
+import 'package:custom_radio_grouped_button/CustomButtons/ButtonTextStyle.dart';
+import 'package:custom_radio_grouped_button/CustomButtons/CustomCheckBoxGroup.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:shufflechat/models/UserData.dart';
+import 'package:provider/provider.dart';
+import 'package:shufflechat/services/authProvider.dart';
+import 'package:shufflechat/services/dbProvider.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+class FilterIntrests extends StatefulWidget {
+  @override
+  _FilterIntrestsState createState() => _FilterIntrestsState();
+}
+
+class _FilterIntrestsState extends State<FilterIntrests> {
+  TextEditingController _intrestField = TextEditingController();
+  bool _isError = false;
+  bool _isVisible = true;
+  bool _intrestsChanged = false;
+
+  void showField() {
+    setState(() {
+      _isVisible = true;
+    });
+  }
+
+  void hideField() {
+    setState(() {
+      _isVisible = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var userData = Provider.of<UserData>(context);
+    _intrestField.text = '';
+
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 6,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15.0, top: 30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SafeArea(
+                      child: GestureDetector(
+                        onTap: () async {
+                          if (userData.interests != null &&
+                              userData.interests.length > 2) {
+                            if (_intrestsChanged) {
+                              _isError = false;
+                              final uid = context.read<AuthProvider>().getUID();
+                              context
+                                  .read<UserData>()
+                                  .setUserDataModel(userData);
+                              await context
+                                  .read<DatabaseProvider>()
+                                  .setUser(uid, userData);
+                            }
+                            Navigator.pop(context);
+                          } else {
+                            _isError = true;
+                            showField();
+                          }
+                        },
+                        child: Icon(
+                          Icons.arrow_back,
+                          color: Colors.black,
+                          size: 35.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 65,
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    'yourIntrests'.tr(),
+                    style:
+                        TextStyle(fontSize: 60.0, fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 25,
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    'chooseIntrests'.tr(),
+                    style:
+                        TextStyle(fontSize: 23.0, fontWeight: FontWeight.w400),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              dragStartBehavior: DragStartBehavior.start,
+              onTapDown: (_) => hideField(),
+              child: Container(
+                width: MediaQuery.of(context).size.width / 1.02,
+                height: MediaQuery.of(context).size.height / 1.6,
+                child: CustomCheckBoxGroup(
+                  buttonTextStyle: ButtonTextStyle(
+                    selectedColor: Colors.white,
+                    unSelectedColor: Colors.black,
+                    textStyle: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  defaultSelected: userData.interests,
+                  unSelectedColor: Theme.of(context).canvasColor,
+                  unSelectedBorderColor: Colors.black,
+                  selectedColor: const Color(0xffff9600),
+                  selectedBorderColor: const Color(0xffff9600),
+                  checkBoxButtonValues: (values) {
+                    userData.interests = values;
+                    if (values.length > 5) {
+                      values.removeLast();
+                    }
+                    _intrestsChanged = true;
+                  },
+                  buttonLables: [
+                    'music'.tr(),
+                    'sport'.tr(),
+                    'fitness'.tr(),
+                    'jogging'.tr(),
+                    'cooking'.tr(),
+                    'baking'.tr(),
+                    'videogames'.tr(),
+                    'seriesAndMovies'.tr(),
+                    'hiking'.tr(),
+                    'dancing'.tr(),
+                    'photography'.tr(),
+                    'technology'.tr(),
+                    'programming'.tr(),
+                    'basketball'.tr(),
+                    'football'.tr(),
+                    'fashion'.tr(),
+                    'singing'.tr(),
+                    'art'.tr(),
+                    'guitar'.tr(),
+                    'instruments'.tr(),
+                    'chess'.tr(),
+                    'animations'.tr(),
+                    'fishing'.tr(),
+                    'martialArts'.tr(),
+                    'reading'.tr(),
+                    'writing'.tr(),
+                    'drawing'.tr(),
+                    'travel'.tr(),
+                    'astrologicalSigns'.tr(),
+                    'yoga'.tr(),
+                    'meditation'.tr(),
+                    'vegan'.tr(),
+                    'vegetarian'.tr(),
+                    'alcohol'.tr(),
+                    'politics'.tr(),
+                    'feminism'.tr(),
+                    'lgbtq+'.tr(),
+                    'environmentalism'.tr(),
+                    'golf'.tr(),
+                    'skiSnowboard'.tr(),
+                    'skating'.tr(),
+                    'dogs'.tr(),
+                    'cats'.tr(),
+                    'gardening'.tr(),
+                    'penPaper'.tr(),
+                    'boardGames'.tr(),
+                    'cycling'.tr(),
+                    'climbing'.tr(),
+                    'craftsmanship'.tr(),
+                    'collections'.tr(),
+                    'extremeSports'.tr(),
+                  ],
+                  buttonValuesList: [
+                    'music',
+                    'sport',
+                    'fitness',
+                    'jogging',
+                    'cooking',
+                    'baking',
+                    'videogames',
+                    'seriesAndMovies',
+                    'hiking',
+                    'dancing',
+                    'photography',
+                    'technology',
+                    'programming',
+                    'basketball',
+                    'footballSoccer',
+                    'fashion',
+                    'singing',
+                    'art',
+                    'guitar',
+                    'instruments',
+                    'chess',
+                    'animations',
+                    'fishing',
+                    'martialArts',
+                    'reading',
+                    'writing',
+                    'drawing',
+                    'travel',
+                    'astrologicalSigns',
+                    'yoga',
+                    'meditation',
+                    'vegan',
+                    'vegetarian',
+                    'alcohol',
+                    'politics',
+                    'feminism',
+                    'lgbtq+',
+                    'environmentalism',
+                    'golf',
+                    'skiSnowboard',
+                    'skating',
+                    'dogs',
+                    'cats',
+                    'gardening',
+                    'penPaper',
+                    'boardGames',
+                    'cycling',
+                    'climbing',
+                    'craftsmanship',
+                    'collections',
+                    'extremeSports',
+                  ],
+                  spacing: 0,
+                  horizontal: true,
+                  enableButtonWrap: false,
+                  enableShape: true,
+                  width: 40,
+                  absoluteZeroSpacing: false,
+                  padding: 5,
+                ),
+              ),
+            ),
+            Container(
+              height: 25,
+              child: Center(
+                child: Visibility(
+                  visible: _isVisible,
+                  child: Text(
+                    _isError ? 'choose3Intrests'.tr() : 'scroll'.tr(),
+                    style: TextStyle(
+                        color: _isError ? Colors.red : Color(0xffff9600),
+                        fontSize: 17,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
--- lib/ui/shuffleScreen.dart
+++ lib/ui/shuffleScreen.dart
@@ -21,195 +21,190 @@ class _ShuffleScreenState extends State<ShuffleScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 30, right: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SafeArea(
-                      child: GestureDetector(
-                        onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Settings(),
-                          ),
-                        ),
-                        child: Icon(
-                          Icons.settings,
-                          color: Colors.black,
-                          size: 35.0,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height / 3,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width / 1.6,
-                      height: 55,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15.0),
-                        color: const Color(0xffff9600),
-                      ),
-                      child: MaterialButton(
-                        textColor: Colors.white,
-                        onPressed: () {
-                          Navigator.push(
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 30, right: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SafeArea(
+                        child: GestureDetector(
+                          onTap: () => Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => ChatScreen(),
+                              builder: (context) => Settings(),
                             ),
-                          );
-                        },
-                        child: Text(
-                          'shuffle',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w700),
+                          ),
+                          child: Icon(
+                            Icons.settings,
+                            color: Colors.black,
+                            size: 35.0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width / 1.6,
+                        height: 55,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          color: const Color(0xffff9600),
+                        ),
+                        child: MaterialButton(
+                          textColor: Colors.white,
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ChatScreen(),
+                              ),
+                            );
+                          },
+                          child: Text(
+                            'shuffle',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      )
+                    ],
+                  ),
+                ),
+                SafeArea(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    height: MediaQuery.of(context).size.height / 3,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: const Color(0xffff9600).withOpacity(0.2),
+                    ),
+                    child: Center(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width / 1.2,
+                        height: MediaQuery.of(context).size.height / 3.2,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              'genderFilter'.tr(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500, fontSize: 18),
+                            ),
+                            Container(
+                              child: CustomRadioButton(
+                                wrapAlignment: WrapAlignment.center,
+                                elevation: 0,
+                                width: 80,
+                                buttonTextStyle: ButtonTextStyle(
+                                  selectedColor: Colors.white,
+                                  unSelectedColor: Colors.black,
+                                  textStyle: TextStyle(
+                                    fontSize: 13,
+                                  ),
+                                ),
+                                unSelectedColor: Theme.of(context).canvasColor,
+                                unSelectedBorderColor:
+                                    Theme.of(context).canvasColor,
+                                selectedColor: const Color(0xffff9600),
+                                selectedBorderColor: const Color(0xffff9600),
+                                radioButtonValue: (value) {
+                                  selectedGender = value;
+                                },
+                                buttonLables: [
+                                  'any'.tr(),
+                                  'female'.tr(),
+                                  'male'.tr(),
+                                  'others'.tr(),
+                                ],
+                                buttonValues: [
+                                  9,
+                                  0,
+                                  1,
+                                  2,
+                                ],
+                                spacing: 0,
+                                defaultSelected: 9,
+                                horizontal: false,
+                                enableButtonWrap: false,
+                                enableShape: true,
+                                height: 40,
+                                absoluteZeroSpacing: false,
+                                padding: 1,
+                              ),
+                            ),
+                            Text(
+                              'ageFilter'.tr(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500, fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 40,
+                              child: FlutterSlider(
+                                rangeSlider: true,
+                                values: [18, 60],
+                                min: 18,
+                                max: 60,
+                                onDragCompleted:
+                                    (handlerIndex, lowerValue, upperValue) {
+                                  selectedMinAge = lowerValue;
+                                  selectedMaxAge = upperValue;
+                                },
+                                trackBar: FlutterSliderTrackBar(
+                                    activeTrackBar: BoxDecoration(
+                                        color: Color(0xffff9600))),
+                                tooltip: FlutterSliderTooltip(
+                                    format: (value) {
+                                      if (value == '60.0') {
+                                        return '60+';
+                                      }
+                                      return value.replaceAll('.0', '');
+                                    },
+                                    positionOffset:
+                                        FlutterSliderTooltipPositionOffset(
+                                            top: -10),
+                                    textStyle: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 17,
+                                        color: Colors.white),
+                                    boxStyle: FlutterSliderTooltipBox(
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(50.0),
+                                            color: Color(0xffffac38)
+                                                .withOpacity(0.9)))),
+                              ),
+                            ),
+                            Text(
+                              'intrestFilter'.tr(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500, fontSize: 18),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: 20,
-                    )
-                  ],
-                ),
-              ),
-              SafeArea(
-                child: Container(
-                  width: MediaQuery.of(context).size.width / 1.1,
-                  height: MediaQuery.of(context).size.height / 3,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50.0),
-                    color: const Color(0xffff9600).withOpacity(0.2),
                   ),
-                  child: Center(
-                    child: Container(
-                      width: MediaQuery.of(context).size.width / 1.2,
-                      height: MediaQuery.of(context).size.height / 3.2,
-                      child: Column(
-                        children: [
-                          Text(
-                            'gender'.tr(),
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500, fontSize: 18),
-                          ),
-                          SizedBox(
-                            height: 6,
-                          ),
-                          Container(
-                            child: CustomRadioButton(
-                              wrapAlignment: WrapAlignment.center,
-                              elevation: 0,
-                              width: 80,
-                              buttonTextStyle: ButtonTextStyle(
-                                selectedColor: Colors.white,
-                                unSelectedColor: Colors.black,
-                                textStyle: TextStyle(
-                                  fontSize: 13,
-                                ),
-                              ),
-                              unSelectedColor: Theme.of(context).canvasColor,
-                              unSelectedBorderColor:
-                                  Theme.of(context).canvasColor,
-                              selectedColor: const Color(0xffff9600),
-                              selectedBorderColor: const Color(0xffff9600),
-                              radioButtonValue: (value) {
-                                selectedGender = value;
-                              },
-                              buttonLables: [
-                                'any'.tr(),
-                                'female'.tr(),
-                                'male'.tr(),
-                                'others'.tr(),
-                              ],
-                              buttonValues: [
-                                9,
-                                0,
-                                1,
-                                2,
-                              ],
-                              spacing: 0,
-                              defaultSelected: 9,
-                              horizontal: false,
-                              enableButtonWrap: false,
-                              enableShape: true,
-                              height: 40,
-                              absoluteZeroSpacing: false,
-                              padding: 1,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 6,
-                          ),
-                          Text(
-                            'ageFilter'.tr(),
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500, fontSize: 18),
-                          ),
-                          SizedBox(
-                            height: 40,
-                            child: FlutterSlider(
-                              rangeSlider: true,
-                              values: [18, 80],
-                              min: 18,
-                              max: 80,
-                              onDragCompleted:
-                                  (handlerIndex, lowerValue, upperValue) {
-                                selectedMinAge = lowerValue;
-                                selectedMaxAge = upperValue;
-                              },
-                              trackBar: FlutterSliderTrackBar(
-                                  activeTrackBar:
-                                      BoxDecoration(color: Color(0xffff9600))),
-                              tooltip: FlutterSliderTooltip(
-                                  format: (value) {
-                                    return value.replaceAll('.0', '');
-                                  },
-                                  positionOffset:
-                                      FlutterSliderTooltipPositionOffset(
-                                          top: -10),
-                                  textStyle: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 17,
-                                      color: Colors.white),
-                                  boxStyle: FlutterSliderTooltipBox(
-                                      decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(50.0),
-                                          color: Color(0xffffac38)
-                                              .withOpacity(0.9)))),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 6,
-                          ),
-                          Text(
-                            'intrestFilter'.tr(),
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500, fontSize: 18),
-                          ),
-                          Container(
-                            height: 10,
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
